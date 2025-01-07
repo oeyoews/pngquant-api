6f67bbc8548cac2197be28e10a1b5337c0957e11
ydj <yaodj@sino-bridge.com>
2025-01-07 09:11:26 +0800

    feat: support cors for api

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['pngquant-bin']
+  serverExternalPackages: ['pngquant-bin'],
   // serverRuntimeConfig: {} as NextConfig,
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Set your origin
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
