43562c03ed314f0aeb09e7b7d85ff809ac601a8c
ydj <yaodj@sino-bridge.com>
2025-01-07 20:04:40 +0800

    fix: update config

--- next.config.ts
+++ next.config.ts
@@ -4,27 +4,27 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ['pngquant-bin'],
   // serverRuntimeConfig: {} as NextConfig,
   /* config options here */
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*', // Set your origin
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Content-Type, Authorization',
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       headers: [
+  //         {
+  //           key: 'Access-Control-Allow-Origin',
+  //           value: '*', // Set your origin
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Methods',
+  //           value: 'GET, POST, PUT, DELETE, OPTIONS',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Headers',
+  //           value: 'Content-Type, Authorization',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
